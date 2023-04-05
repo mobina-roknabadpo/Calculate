ad4f81c7cd38b09d70d25ac89fb2223f11f4710a
Mobina Roknabadpo <mobina.rp1380@gmail.com>
2023-04-05 19:28:56 +0330

    feat: add button for erase single digit

--- script.js
+++ script.js
@@ -3,9 +3,10 @@
     const buttons = document.querySelectorAll('.btn');
     const equal = document.querySelector('.equal');
     const clear = document.querySelector('.clear');
+    const del = document.querySelector('.delete');
     let equal_pressed = 0;
-    window.onload = () =>{
-        screen.value = '';
+    window.onload = () => {
+        screen.value = "";
     }
     buttons.forEach((button) => {
         button.addEventListener('click', (e) => {
@@ -23,5 +24,8 @@
 
     clear.addEventListener('click', () => {
         screen.value = " ";
+    })
+    del.addEventListener('click', () => {
+        screen.value = screen.value.substring(0, screen.value.length - 1);
     })
 })();
